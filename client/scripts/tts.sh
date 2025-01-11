9cb13c0a2507963471f91bc624247503ee6bdd48
Ash Martian <ash.lee.martian@gmail.com>
2025-01-11 06:05:58 +0000

    Be sire to have proper checkpoint location

--- client/scripts/tts.sh
+++ client/scripts/tts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "$1" | \
-  ~/.local/bin/piper --model ../checkpoints/en_US-amy-medium.onnx --output-raw | \
+  ~/.local/bin/piper --model ./checkpoints/en_US-amy-medium.onnx --output-raw | \
   aplay -r 22050 -f S16_LE -t raw -
